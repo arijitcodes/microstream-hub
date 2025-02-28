af97398cfa5f9dcba5a95f867357e82e280a6875
Arijit Banerjee <72866688+arijitcodes@users.noreply.github.com>
2025-03-01 03:38:18 +0530

    feat(semantic-release): set up Semantic-Release for auto Release & Update
        Setup Semantic-Release with Commitizen - along with GitHub Workflow. Now this setup will -
    
         - Auto Release on GitHub, NPM
         - Update Changelog automatically from Conventional Commit messages and update version in Package.json
         - Set up Commitizen for Conventiona Commit message formatting
         - Setup GitHub Workflow to automate all these tasks
    
    fix #1

---
+++ release.config.js
@@ -0,0 +1,47 @@
+const { readFileSync } = require("fs");
+const { join } = require("path");
+
+module.exports = {
+  branches: ["main"],
+  repositoryUrl: "git@github.com:arijitcodes/microstream-hub.git", // Update for the Hub
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    [
+      "@semantic-release/release-notes-generator",
+      {
+        writerOpts: {
+          commitPartial: readFileSync(
+            join(__dirname, "./configs/semantic-release/commit-template.hbs"),
+            "utf-8"
+          ),
+        },
+      },
+    ],
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "CHANGELOG.md",
+      },
+    ],
+    [
+      "@semantic-release/github",
+      {
+        assets: [
+          {
+            path: "microstream-hub.tar.gz", // The archive created by packGithubBuild
+            name: "microstream-hub_v${nextRelease.version}.tar.gz",
+            label: "MicroStream Hub v${nextRelease.version}",
+          },
+        ],
+      },
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md", "package*.json", "bun.lockb"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+  ],
+};
